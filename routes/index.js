b4e9f5d3536a37097fe36686f4884123199475bd
ccp-linx <601211505@qq.com>
2019-06-23 20:09:46 +0800

    登录页面和第一页面

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var user = {};
+  res.render('index', { title: 'Express', 'user':user });
+});
+
+router.get('/step1', function(req, res, next) {
+  res.render('step1', { title: 'Express','jbHours':'12','jbDays':'33' });
+});
+
+router.get('/step2', function(req, res, next) {
+  res.render('step2', { title: 'Express' });
+});
+
+router.get('/step3', function(req, res, next) {
+  res.render('step3', { title: 'Express' });
+});
+
+router.get('/step4', function(req, res, next) {
+  res.render('step4', { title: 'Express' });
 });
 
 module.exports = router;
